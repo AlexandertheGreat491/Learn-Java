48757d26e42668e7715986daf60956c975450ab6
agvan <agvandyke25@gmail.com>
2023-03-04 12:02:44 -0600

    printing numbers

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main {
     System.out.println("Hello World!");
     System.out.println("I am learning Java.");
     System.out.println("It is awesome!");
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
   }
 }
