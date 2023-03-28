5ea90341b585d6aabdc2d3c7b658aae1f387895b
agvan <agvandyke25@gmail.com>
2023-03-27 21:19:05 -0500

    added one comment

--- Main.java
+++ Main.java
@@ -80,6 +80,7 @@ public class Main {
      System.out.println(n == 12); // returns true, because n equals 12
      // a boolean expression that I know will return false
      System.out.println(10 == 15);
+     // real life example
   }
 }
 // All Java variables must be identified with unique names.
