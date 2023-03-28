d3fd688349c06a57e42717b44745024aff8a55db
agvan <agvandyke25@gmail.com>
2023-03-27 21:11:54 -0500

    added another single line comment

--- Main.java
+++ Main.java
@@ -78,6 +78,7 @@ public class Main {
      */
      int n = 12;
      System.out.println(n == 12); // returns true, because n equals 12
+     // a boolean expression that I know will return false
   }
 }
 // All Java variables must be identified with unique names.
